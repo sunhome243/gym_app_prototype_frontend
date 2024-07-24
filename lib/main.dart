724dee96bc6f7384a942ebcf2c02ef6d7b4c1473
sunho <sunhome243@gmail.com>
2024-07-24 14:19:14 +0900

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'screens/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
   runApp(const MyApp());
