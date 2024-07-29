349dbbc868857768b87e6ae2d56e5a446e2a6ab6
sunho <sunhome243@gmail.com>
2024-07-29 15:41:41 +0900

    fix

--- gym-app-prototype-front/flutter_application_1/lib/services/config.dart
+++
@@ -1,7 +0,0 @@
-class Config {
-  static const String userServiceUrl = 'http://127.0.0.1:8000';
-  static const String workoutServiceUrl = 'http://127.0.0.1:8001';
-  static const String statsServiceUrl = 'http://127.0.0.1:8002';
-
-  // 다른 설정들을 여기에 추가할 수 있습니다.
-}
